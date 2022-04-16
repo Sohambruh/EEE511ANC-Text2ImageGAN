e42a298e0ffae109b7cc21218dcd26f99b2f4de5
Sohambruh <76569394+Sohambruh@users.noreply.github.com>
2022-04-16 21:12:49 +0530

    Update runtime.py

--- PyTorch/runtime.py
+++ PyTorch/runtime.py
@@ -2,44 +2,26 @@ from trainer import Trainer
 import argparse
 from PIL import Image
 import os
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--type", default='gan')
-parser.add_argument("--lr", default=0.0002, type=float)
-parser.add_argument("--l1_coef", default=50, type=float)
-parser.add_argument("--l2_coef", default=100, type=float)
-parser.add_argument("--diter", default=5, type=int)
-parser.add_argument("--cls", default=False, action='store_true')
-parser.add_argument("--vis_screen", default='gan')
-parser.add_argument("--save_path", default='')
-parser.add_argument("--inference", default=False, action='store_true')
-parser.add_argument('--pre_trained_disc', default=None)
-parser.add_argument('--pre_trained_gen', default=None)
-parser.add_argument('--dataset', default='flowers')
-parser.add_argument('--split', default=0, type=int)
-parser.add_argument('--batch_size', default=64, type=int)
-parser.add_argument('--num_workers', default=8, type=int)
-parser.add_argument('--epochs', default=10, type=int)
-args = parser.parse_args()
-
-trainer = Trainer(type=args.type,
-                  dataset=args.dataset,
-                  split=args.split,
-                  lr=args.lr,
-                  diter=args.diter,
-                  vis_screen=args.vis_screen,
-                  save_path=args.save_path,
-                  l1_coef=args.l1_coef,
-                  l2_coef=args.l2_coef,
-                  pre_trained_disc=args.pre_trained_disc,
-                  pre_trained_gen=args.pre_trained_gen,
-                  batch_size=args.batch_size,
-                  num_workers=args.num_workers,
-                  epochs=args.epochs
-                  )
-
-if not args.inference:
-    trainer.train(args.cls)
-else:
-    trainer.predict()
-
+def main(type_m='gan',lr=0.0002,l1_coef=50,l2_coef=50,diter=5,cls_m=False,vis_screen='gan',save_path='',inference=False,pre_trained_disc=None,pre_trained_gen=None,dataset='birds',split=0,batch_size=64,num_workers=8,epochs=10):
+    
+    
+    trainer = Trainer(type=type_m,
+                      dataset=dataset,
+                      split=split,
+                      lr=lr,
+                      diter=diter,
+                      vis_screen=vis_screen,
+                      save_path=save_path,
+                      l1_coef=l1_coef,
+                      l2_coef=l2_coef,
+                      pre_trained_disc=pre_trained_disc,
+                      pre_trained_gen=pre_trained_gen,
+                      batch_size=batch_size,
+                      num_workers=num_workers,
+                      epochs=epochs
+                      )
+    
+    if not args.inference:
+        trainer.train(cls_m)
+    else:
+        trainer.predict()
